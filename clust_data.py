16974069c9a48c143c9117eae38255c900e98231
migbro <miguel.a.brown@gmail.com>
2016-08-01 12:43:20 -0500

    reinstated limit on height

--- clust_data.py
+++ clust_data.py
@@ -157,8 +157,8 @@ sys.stderr.write('Clustering and drawing figures\n')
 (r, c) = data.shape
 if r > 6:
     r = math.ceil(r / 2)
-#if c > 6:
-#    c = math.ceil(c / 6)
+if c > 6:
+    c = math.ceil(c / 6)
 
 sys.stderr.write('Dimensions set as width ' + str(c) + ' height ' + str(r) + '\n')
 # if custom colormap supplied, use it
